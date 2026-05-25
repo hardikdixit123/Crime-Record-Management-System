a2f65a837ec8830754840992576993b5e4d1ad35
hardikdixit123 <hardikdixit123@gmail.com>
2026-05-26 00:30:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 
-
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "root123",
+    "password": "YOUR_MYSQL_PASSWORD",
     "database": "crms_db"
 }
 
-SECRET_KEY = "some_really_random_secret_key_here"
-
-
-SECRET_KEY = "some_really_random_secret_key_here"
+SECRET_KEY = "change_this_to_any_random_long_string"
